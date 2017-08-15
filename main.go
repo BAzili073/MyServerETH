f4be6e4d2bc91bbb4e8d68826c787cb21fa5b47c
bazili073 <bazili073@ya.ru>
2017-08-15 16:24:02 +0300

    tcp server + web server(not work)

--- main.go
+++ main.go
@@ -8,23 +8,36 @@ import (
     "net"
     "html/template"
     "log"
+    "encoding/json"
     // "time"
     "os"
+    "./arduino"
+    "strconv"
 )
 
 const (
-    CONN_HOST = "localhost"
-    CONN_PORT = "3333"
-    CONN_TYPE = "tcp"
+    ID  = 0
+    COMMAND = 1
+    ARG_1 = 2
+    ARG_2  = 3
 )
+
+type Point struct{
+  Ip string
+  Port string
+  Id byte
+  Conn *net.Conn
+
+}
 
 type page struct {
   Title string
   Msg string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love  %s!", r.URL.Path[1:])
+type rgbResponse struct {
+  Value string `json:"value"`
+  Id string `json:"id"`
 }
 
 func index(w http.ResponseWriter, r *http.Request){
@@ -32,48 +45,86 @@ func index(w http.ResponseWriter, r *http.Request){
   t, err := template.ParseFiles("./index.html")
   if err !=nil {log.Panic(err)}
   t.Execute(w, &page{Title:"Just Page",Msg: "Just Message"});
-  log.Printf("hello");
 }
 
 func main() {
-  // Listen for incoming connections.
- l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
- if err != nil {
-     fmt.Println("Error listening:", err.Error())
-     os.Exit(1)
- }
- // Close the listener when the application closes.
- defer l.Close()
- fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
- for {
-     // Listen for an incoming connection.
-     conn, err := l.Accept()
-     if err != nil {
-         fmt.Println("Error accepting: ", err.Error())
-         os.Exit(1)
-     }
-     // Handle connections in a new goroutine.
-     go handleRequest(conn)
- }
+  go webServer();
+  go tcpServer();
 
-  http.HandleFunc("/", index)
-  http.ListenAndServe(":8080", nil)
-
+  var input string
+  fmt.Scanln(&input)
 }
 
-func handleRequest(conn net.Conn) {
-  // Make a buffer to hold incoming data.
-  buf := make([]byte, 1024)
-  // Read the incoming connection into the buffer.
-  reqLen, err := conn.Read(buf)
-  if err != nil {
-    log.Println("Error reading:", err.Error())
-  }else{
-    log.Printf("Len = %d",reqLen);
-    log.Printf("Buff = %s",buf);
+func webServer(){
+  for {
+    http.HandleFunc("/", index)
+    http.HandleFunc("/api/v1/values", changeRGB)
+    http.ListenAndServe(":8080", nil)
   }
-  // Send a response back to person contacting us.
-  conn.Write([]byte("Message received."))
-  // Close the connection when you're done with it.
-  // conn.Close()
 }
+
+func changeRGB (w http.ResponseWriter, r *http.Request){
+  fmt.Println("cnhage")
+  decoder := json.NewDecoder(r.Body)
+    var color rgbResponse
+    err := decoder.Decode(&color)
+    if err != nil {
+      log.Fatal(err)
+    }
+    colorValue, err := strconv.Atoi(color.Value);
+    if (color.Id == "Red"){
+      // arduino.DigitalWrite(Point_1.Conn,1,1)
+      fmt.Println("Red = ",colorValue)
+    }
+}
+
+func tcpServer(){
+  Point_1 := Point{
+    Ip : "10.10.2.9",
+    Port : "3333",
+    Id : 9};
+  p1, err := net.Listen("tcp", ":"+Point_1.Port)
+  if err != nil {
+      fmt.Println("Error listening:", err.Error())
+      os.Exit(1)
+  }
+  // Close the listener when the application closes.
+  defer p1.Close()
+  fmt.Println("Listening on "  + Point_1.Port + " port")
+  for {
+      // Listen for an incoming connection.
+      conn, err := p1.Accept()
+      Point_1.Conn = &conn;
+      if err != nil {
+          fmt.Println("Error accepting: ", err.Error())
+          os.Exit(1)
+      }else{
+        fmt.Println("Connection accept")
+      }
+      // Handle connections in a new goroutine.
+      go point1Server(conn)
+     //  l.Close();
+  }
+}
+
+func point1Server(conn net.Conn) {
+  for{
+      buf := make([]byte, 5)
+      reqLen, err := conn.Read(buf)
+      if err != nil {
+        // log.Println("Error reading:", err.Error())
+      }else{
+        log.Printf("Len = %d",reqLen);
+        log.Printf("Buff = %d",buf);
+      }
+
+      if (buf[COMMAND] == arduino.COMMAND_SETUP){
+        arduino.SetupPoint_1(conn);
+      }
+    }
+}
+
+// func pinMode(conn net.Conn, pin byte, mode byte){
+//   Send_message := ;
+//
+// }
